0430179569596b2bb8149ccad6bee84503c393ec
Aaron Whang and Erik Irgens <erik@>
2019-06-10 16:49:58 -0700

    add commented out code inside scripts.js that is the program for word play, count to function and card deck.

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,30 @@ var groceryArrays = [];
 
   });
 });
+
+//Word play program- input into console
+// var string = prompt("enter sentence");
+// var sentArray = string.split('');
+// for (var index = 0; index < sentArray.length; index += 1) {
+//     if (sentArray[index] === "a" || sentArray[index] === "e" || sentArray[index] === "i" || sentArray[index] === "o" || sentArray[index] === "u" || sentArray[index] === "A" || sentArray[index] === "E" || sentArray[index] === "I" || sentArray[index] === "O" || sentArray[index] === "U") {
+//         sentArray[index] = "-";
+//     }
+// };
+// console.log(sentArray.join(' '));
+
+
+// count to function
+// var userNum = parseInt(prompt("Enter a number to count to"));
+// var userCount = parseInt(prompt("Enter a number to count by"));
+// for (var current = 0; current <= userNum; current += userCount) {
+// 	console.log(current);
+// }
+//
+//card deck generator
+// var suits = ["spade", "club", "heart", "diamond"];
+// var cards = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+// suits.forEach(function(suit){
+//     cards.forEach(function(card){
+// 		console.log(card + " of " + suit);
+//     });
+// });
